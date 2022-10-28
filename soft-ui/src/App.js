fa736289ca293c08ffd198064b0960d6e13c1c5b
Jacob Habib <habib.50@osu.edu>
2022-10-28 12:56:12 -0400

    Base project w/react, react styleguidist, and tailwind configured

--- soft-ui/src/App.js
+++ soft-ui/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>
+          Hello world!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ soft-ui/src/components/SampleComponent.js
@@ -0,0 +1,7 @@
+const SampleComponent = () => {
+    return (<div className="flex text-purple-400 text-2xl">
+        Hello World
+    </div>)
+}
+
+export default SampleComponent
