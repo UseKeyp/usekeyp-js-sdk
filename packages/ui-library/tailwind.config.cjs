6edd111c5652c0d58b00b6a679233ccfd2b64492
alekseevayana <alekseevayana@bk.ru>
2023-03-28 09:53:11 -0700

    set up tailwindcss and reusing component from soft-ui lib

--- packages/ui-library/tailwind.config.cjs
+++ packages/ui-library/tailwind.config.cjs
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-const { softUi } = require('@usekeyp/tailwind-presets')
+const { softUi } = require("@usekeyp/tailwind-presets");
 
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx,md}",
+    "../components-soft-ui/**/*.{js,jsx}",
   ],
   presets: [softUi],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
