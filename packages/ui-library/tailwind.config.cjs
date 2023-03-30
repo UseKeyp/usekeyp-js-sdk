27cf330f03a4523b821b62830365719cc3541670
alekseevayana <alekseevayana@bk.ru>
2023-03-30 11:01:06 -0700

    remove redundant packages

--- packages/ui-library/tailwind.config.cjs
+++ packages/ui-library/tailwind.config.cjs
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-const { softUi } = require("@usekeyp/tailwind-presets");
 
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx,md}",
-    "../components-soft-ui/**/*.{js,jsx}",
-  ],
-  presets: [softUi],
+  content: ["./src/**/*.{js,jsx,ts,tsx,md}"],
   theme: {
     extend: {},
   },
