f4f3502584cfde46a6818504dc91dff275f3f2a7
alekseevayana <alekseevayana@bk.ru>
2023-05-05 14:11:50 -0700

    fix google icon

--- packages/ui-kit/src/assets/icons/icon-map.js
+++ packages/ui-kit/src/assets/icons/icon-map.js
@@ -193,12 +193,13 @@ const GitHub = ({ loading, disabled }) => {
   );
 };
 
-const Google = ({ loading, disabled }) => {
+const Google = ({ loading, disabled, width, height }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={width}
+      height={height}
+      viewBox="0 0 19 24"
       className={`overflow-visible
         ${loading && "fill-white"}
         ${disabled && !loading && "fill-gray-300"} 
