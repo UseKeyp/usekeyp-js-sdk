df5dfd62ea49ff9647f58346ec0fb145decf79cc
jahabeebs <jahgoreds@gmail.com>
2023-06-28 16:01:28 -0400

    refactor auth code

--- packages/js-sdk/src/keyp-auth.ts
+++ packages/js-sdk/src/keyp-auth.ts
@@ -1,38 +1,15 @@
-type Options = {
-    clientId: string | undefined;
-    secret?: string | undefined;
-    redirectUrl?: string | undefined;
-};
+import { OAuthConfig, OAuthUserConfig } from "next-auth/providers";
 
-type Profile = {
-    sub: string;
-    username: string;
-    address: string;
-    email: string;
-    imageSrc: string;
-};
+export interface KeypProfile extends Record<string, any> {
+    id: string
+    username: string
+    email: string
+    address: string
+}
 
-type Token = {
-    accessToken?: string;
-    username?: string;
-    address?: string;
-    sub?: string;
-};
-
-type Account = {
-    access_token: string;
-};
-
-type Session = {
-    user: {
-        accessToken?: string;
-        username?: string;
-        address?: string;
-        id?: string;
-    };
-};
-
-const KeypProvider = (options: Options) => {
+export function Keyp<P extends KeypProfile>(
+    options: OAuthUserConfig<P>
+): OAuthConfig<P> {
     const { clientId } = options;
 
     const KEYP_API_DOMAIN =
@@ -48,51 +25,14 @@ const KeypProvider = (options: Options) => {
         checks: ["pkce"],
         authorization: { params: { scope: "openid email" } },
         client: { token_endpoint_auth_method: "none" },
-        profile(profile: Profile) {
+        profile(profile) {
             return {
                 id: profile.sub,
                 username: profile.username,
-                address: profile.address,
                 email: profile.email,
-                imageSrc: profile.imageSrc,
+                address: profile.address,
             };
         },
-    };
-};
-
-/**
- * Creates an easy plug-in for NextAuth.js to use Keyp as an authentication provider
- */
-export const KeypAuth = (options: Options) => {
-    const keypProvider = KeypProvider(options);
-
-    return {
-        secret: options.secret,
-        providers: [keypProvider],
-        callbacks: {
-            async jwt({ token, account, profile }: { token: Token; account: Account; profile: Profile }) {
-                if (account) {
-                    // Comes from the returned JWT from Keyp
-                    token.accessToken = account.access_token;
-                }
-                if (profile) {
-                    // Comes from the /userinfo endpoint
-                    token.username = profile.username;
-                    token.address = profile.address;
-                    token.sub = profile.sub;
-                }
-                return token;
-            },
-            async session({ session, token }: { session: Session; token: Token }) {
-                // Send properties to the client, like an access_token from a provider.
-                if (token) {
-                    session.user.accessToken = token.accessToken;
-                    session.user.username = token.username;
-                    session.user.address = token.address;
-                    session.user.id = token.sub;
-                }
-                return session;
-            },
-        },
-    };
-};
+        options,
+    }
+}
